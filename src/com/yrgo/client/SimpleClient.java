694bc33240597369f6597778c14b946f31831ae5
magnus <magnus@connoisseur.se>
2022-11-07 10:59:26 +0100

    finished assignment1

--- src/com/yrgo/client/SimpleClient.java
+++ src/com/yrgo/client/SimpleClient.java
@@ -8,9 +8,8 @@ public class SimpleClient {
     public static void main(String[] args) {
         var container = new ClassPathXmlApplicationContext("application.xml");
         CustomerManagementService service = container.getBean("customerManagementService", CustomerManagementService.class);
-//        service.getAllCustomers().forEach(System.out::println);
+        service.getAllCustomers().forEach(System.out::println);
 
-        service.findCustomersByName("River Ltd").forEach(System.out::println);
     }
 
 }
