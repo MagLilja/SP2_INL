dc9d7468c9b91cef89cfe4eb1aa4d464608c0cd0
magnus <magnus@connoisseur.se>
2022-11-16 13:51:18 +0100

    created PerformanceTimingAdvice Added deps in pom.xml Added aop config etc. in application.xml

---
+++ src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,32 @@
+package com.yrgo.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.AfterReturningAdvice;
+
+public class PerformanceTimingAdvice {
+
+    public Object timeMethod(ProceedingJoinPoint method) throws Throwable {
+
+        //Before our method
+        long startTime = System.nanoTime();
+
+        // run our method
+        try {
+            return method.proceed();
+        }
+        // after our method has executed
+        finally {
+            long endTime = System.nanoTime();
+            long diff = endTime - startTime;
+            long timeToExecuteMethod = diff / 1000000;;
+
+            System.out.printf("The method %s from the class %s took %d ms to execute \n", method.getSignature().getName(),method.getSignature().getDeclaringType(), timeToExecuteMethod);
+        }
+
+    }
+
+    ;
+
+}
