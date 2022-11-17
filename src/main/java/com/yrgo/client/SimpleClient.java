704fd7563a4991b9496173e8a058f8c219c28e97
magnus <magnus@connoisseur.se>
2022-11-17 09:08:38 +0100

    put container in try with resources

--- src/main/java/com/yrgo/client/SimpleClient.java
+++ src/main/java/com/yrgo/client/SimpleClient.java
@@ -15,35 +15,35 @@ import java.util.List;
 public class SimpleClient {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+        try (ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml")) {
 
-        CustomerManagementService customerService = container.getBean(CustomerManagementService.class);
-        CallHandlingService callService = container.getBean(CallHandlingService.class);
-        DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
+            CustomerManagementService customerService = container.getBean(CustomerManagementService.class);
+            CallHandlingService callService = container.getBean(CallHandlingService.class);
+            DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
 
-        customerService.newCustomer(new Customer("CS03939", "Acme", "Good Customer"));
-        Call call = new Call("Call from Acme");
-        Action action = new Action("Call back tomorrow", LocalDate.of(2016, 12, 1),"Customer Harry");
-            Action action1 = new Action("Talk to Harry", LocalDate.of(2016, 12, 10),"Customer Harry");
-        //Testing the call service with invalid customer id
-        List<Action> actionList = List.of(action, action1);
-        try {
-            callService.recordCall("BAD ID", call, actionList);
-        } catch (CustomerNotFoundException e) {
-            System.out.println("Customer not found");
+            customerService.newCustomer(new Customer("CS03939", "Acme", "Good Customer"));
+            Call call = new Call("Call from Acme");
+            Action action = new Action("Call back tomorrow", LocalDate.of(2016, 12, 1), "Customer Harry");
+            Action action1 = new Action("Talk to Harry", LocalDate.of(2016, 12, 10), "Customer Harry");
+            //Testing the call service with invalid customer id
+            List<Action> actionList = List.of(action, action1);
+            try {
+                callService.recordCall("BAD ID", call, actionList);
+            } catch (CustomerNotFoundException e) {
+                System.out.println("Customer not found");
+            }
+
+            //Testing the call service with valid customer id
+            try {
+                callService.recordCall("CS03939", call, actionList);
+            } catch (CustomerNotFoundException e) {
+                System.out.println("Customer not found");
+            }
+
+            //Testing the diary service
+            diaryService.getAllIncompleteActions("Customer Harry").forEach(System.out::println);
         }
 
-        //Testing the call service with valid customer id
-        try {
-            callService.recordCall("CS03939", call, actionList);
-        } catch (CustomerNotFoundException e) {
-            System.out.println("Customer not found");
-        }
-
-        //Testing the diary service
-        diaryService.getAllIncompleteActions("Customer Harry").forEach(System.out::println);
-
-        container.close();
 
     }
 
